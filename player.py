3072d4f6a9be056672c97ee72c08ae7cbb6d4bcb
Nguyen Phuc Binh <binhnguyenlcs@gmail.com>
2026-04-15 14:12:58 +0700

    feat: Implement HumanPlayer class with input validation

---
+++ player.py
@@ -0,0 +1,17 @@
+class HumanPlayer:
+    def __init__(self, letter):
+        letter = letter.upper()
+        if letter not in ["X", "O"]:
+            raise ValueError("Invalid symbol! Player must choose either 'X' or 'O'.")
+        self.letter = letter
+    def get_move(self, board):
+        valid_char = False
+        value = None
+        while not valid_char:
+            pos = input("Its {self.letter} turn! Please enter the position from 0 to 8")
+            try:
+                val = int(pos)
+                valid_char = True
+            except ValueError:
+                print("Wrong Value, please enter a number from 0 to 8")
+        return val
